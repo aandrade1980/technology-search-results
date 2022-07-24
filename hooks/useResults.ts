8df8bedfc54c41592664444841dfe7baa2779328
Alvaro <varito1@gmail.com>
2022-07-24 17:50:09 -0300

    :art: Added React Query. Created custom hook, useResults

---
+++ hooks/useResults.ts
@@ -0,0 +1,25 @@
+/** Hooks */
+import { useQuery } from '@tanstack/react-query';
+
+/** Types */
+import { Result } from '../@types/sharedTypes';
+
+const searchByText = async (text: string): Promise<Array<Result>> => {
+  const response = await fetch(`/api/getResults?searchText=${text}`);
+
+  return await response.json();
+};
+
+export default function useResults(searchText: string) {
+  const { isLoading, data: searchResults } = useQuery(
+    ['searchQuery', searchText],
+    () => searchByText(searchText),
+    {
+      enabled: !!searchText
+    }
+  );
+
+  const isSearching = isLoading && searchText ? true : false;
+
+  return { isSearching, searchResults };
+}
